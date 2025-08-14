317873e82b8138505ad909487d6b400d81636884
peterhtwe53-creator <peterhtwe53@gmail.com>
2025-08-15 00:05:42 +0630

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* Corrected PWA app logic */
 document.addEventListener('DOMContentLoaded', () => {
     // --- PASTE YOUR WEB APP URL FROM GOOGLE APPS SCRIPT HERE ---
-    const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyP-JUwQdcvmC06JktAZZbNu-llOTxxgJ8OGYs2j3ZLpXwUkl9R8LEgdcyFB9Ke0maI/exec";
+    const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyURrRHHR0sUyHu5pQ_vu0_rvtk3_68NeZykX0fhiZ91i_LgKFvbHCYti0rT87C__Sf/exec";
     
     // Get all necessary DOM elements
     const form = document.getElementById("shopForm");
